9b277bbb9cf86eb77a35936ded09ea0fbf2c993a
Artie Humphreys <artieh2003@gmail.com>
2024-06-05 11:01:37 -0400

    feat: modeling the position, velocity, and acceleration of markers

--- script.py
+++ script.py
@@ -1,13 +1,16 @@
 from viconnexusapi import ViconNexus
 import os
+import numpy as np
+import matplotlib.pyplot as plt
 
 vicon = ViconNexus.ViconNexus()
-player_file = "Play-30"
-# for file in os.path(f"C:/Users/ahumphreys/EXOS_Processing/{player_file}"):
-#     vicon.OpenTrial(file, 30)
+def open_file():
+    player_file = "Play-30"
+    # for file in os.path(f"C:/Users/ahumphreys/EXOS_Processing/{player_file}"):
+    #     vicon.OpenTrial(file, 30)
 
-file = fr"C:\Users\ahumphreys\EXOS_Processing\{player_file}\Cleat01\{player_file.replace('-', '')}_Cleat01_Trial06"
-vicon.OpenTrial(file, 30)
+    file = fr"C:\Users\ahumphreys\EXOS_Processing\{player_file}\Cleat01\{player_file.replace('-', '')}_Cleat01_Trial06"
+    vicon.OpenTrial(file, 30)
 
 subject = vicon.GetSubjectNames()[0]
 
@@ -30,7 +33,33 @@ left_foot_markers = (
 
 
 z_coords = {marker: [] for marker in right_foot_markers}
+velo = {marker: [] for marker in right_foot_markers}
+accel = {marker: [] for marker in right_foot_markers}
+jerk = {marker: [] for marker in right_foot_markers}
 
 for marker in right_foot_markers:
     z_coords[marker].extend(vicon.GetTrajectoryAtFrame(subject, marker, frame)[2] for frame in range(user_defined_region[0], user_defined_region[1]))
-print(z_coords)
+for marker in right_foot_markers:   
+    velo[marker].extend(np.gradient(z_coords[marker]))
+for marker in right_foot_markers:   
+    accel[marker].extend(np.diff(z_coords[marker], 2))
+
+is_striking = True
+
+def plot():
+    plt.figure(figsize=(10,6))
+
+
+    frames = np.arange(user_defined_region[0], user_defined_region[1])
+    plt.plot(frames, z_coords['RD2P'])
+    plt.plot(frames, velo['RD2P'], label = 'velo')
+    frames = np.arange(user_defined_region[0], user_defined_region[1] - 2)
+    plt.plot(frames, accel['RD2P'], label = 'accel')
+
+    plt.xlabel('Frame')
+    plt.title('Kinematics over time')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+plot()
