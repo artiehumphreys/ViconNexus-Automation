fix: new method for determining foot-up actions (change in sign of jerk after peak or trough in velocity, whichever comes first)

script.py
@@ -2,6 +2,7 @@ from viconnexusapi import ViconNexus
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.signal import argrelextrema
 
 vicon = ViconNexus.ViconNexus()
 player_file = "Play-30"
@@ -48,74 +49,66 @@ for marker in right_foot_markers:
 for marker in right_foot_markers:   
     jounce[marker].extend(np.diff(z_coords[marker], 4))
 
-is_striking = True
-def find_strikes(marker: str = 'RHEE'):
+def find_cycles(marker: str = 'RD2P'):
+    foot_down = False
     def is_accel_peak(i, threshold: float =5):
         return marker_accel[i] > threshold and marker_accel[i-1] < marker_accel[i] > marker_accel[i+1]
  
     def is_velo_trough(i, threshold: float =-5):
         return marker_velo[i] < threshold and marker_velo[i-1] > marker_velo[i] < marker_velo[i+1]
+    
+    def is_velo_peak(i, threshold: float =5):
+        return marker_velo[i] > threshold and marker_velo[i-1] < marker_velo[i] > marker_velo[i+1]
  
-    def is_velo_peak(i):
-        return marker_velo[i] > 5 and marker_velo[i-1] < marker_velo[i] > marker_velo[i+1]
- 
-    foot_down = False
-    accel_peak = velo_trough_for_plant = velo_trough_for_footup = velo_peak = accel_peak_for_footup = False
- 
+    accel_peak = velo_trough = velo_trough_plant = velo_peak = False
     marker_accel = accel[marker]
     marker_velo = velo[marker]
     marker_jerk = jerk[marker]
-
-    print(is_velo_trough(679 - user_defined_region[0], -2.5), is_accel_peak(680 - user_defined_region[0], 2.5))
  
     for i in range(1, len(marker_accel) - 1):
-
-        if foot_down:
-            if is_accel_peak(i, 0.75):
-                accel_peak_for_footup = True
-            if is_velo_trough(i, -0.75):
-                velo_trough_for_footup = True
-            if is_velo_peak(i):
-                velo_peak = True
-            
-            if accel_peak_for_footup and velo_trough_for_footup and marker_accel[i-1] > marker_velo[i-1] and marker_accel[i] < marker_velo[i]:
-                print("foot up action at " + str(i + user_defined_region[0]))
-                foot_down = False
-                accel_peak_for_footup = velo_trough_for_footup = False
-            elif velo_peak and marker_jerk[i] > marker_accel[i]:
-                print("foot up at " + str(i + user_defined_region[0]))
-                foot_down = False
-                velo_peak = False
-        else:
+        if not foot_down:
             if is_accel_peak(i):
                 accel_peak = True
-    
-            if is_velo_trough(i):
-                velo_trough_for_plant = True
-
-            if accel_peak and velo_trough_for_plant and marker_accel[i-1] > marker_velo[i-1] and marker_accel[i] < marker_velo[i]:
-                print("strike at " + str(i-1 + user_defined_region[0]))
-                accel_peak = velo_trough_for_plant = False
-                foot_down = True
-
         
-        # if not accel_peak and not velo_trough_for_plant and not velo_peak and marker_velo[i] > 5:
-        #     velo_peak = False
-        # if velo_peak and marker_velo[i-1] > marker_jerk[i-1] and marker_velo[i] < marker_jerk[i]:
-        #     print("foot up at " + str(i - 1 + user_defined_region[0]))
-        #     velo_peak = False
+            if is_velo_trough(i):
+                velo_trough = True
 
-def plot():
+            if accel_peak and velo_trough and marker_accel[i-1] > marker_velo[i-1] and marker_accel[i] < marker_velo[i]:
+                print("strike at " + str(i-1 + user_defined_region[0]))
+                accel_peak = velo_trough = False
+                foot_down = True
+        else:
+            if is_velo_peak(i):
+                velo_peak = True
+        
+            if is_velo_trough(i, -2.5):
+                velo_trough_plant = True
+            
+            if velo_peak and marker_jerk[i] > 0:
+                print("foot up at " + str(i-1 + user_defined_region[0]))
+                velo_peak = velo_trough_plant = False
+                foot_down = False
+
+            elif velo_trough_plant and marker_jerk[i] > 0:
+                print("foot up at " + str(i-1 + user_defined_region[0]))
+                velo_peak = velo_trough_plant = False
+                foot_down = False
+
+    
+
+def plot(marker: str = 'RD2P'):
     plt.figure(figsize=(10,6))
 
 
     frames = np.arange(user_defined_region[0], user_defined_region[1])
-    plt.plot(frames, z_coords['RD2P'])
-    plt.plot(frames, velo['RD2P'], label = 'velo')
+    plt.plot(frames, z_coords[marker])
+    plt.plot(frames, velo[marker], label = 'velo')
     frames = np.arange(user_defined_region[0], user_defined_region[1] - 2)
-    plt.plot(frames, accel['RD2P'], label = 'accel')
+    plt.plot(frames, accel[marker], label = 'accel')
     frames = np.arange(user_defined_region[0], user_defined_region[1] - 3)
-    plt.plot(frames, jerk['RD2P'], label = 'jerk')
+    plt.plot(frames, jerk[marker], label = 'jerk')
+    frames = np.arange(user_defined_region[0], user_defined_region[1] - 4)
+    plt.plot(frames, jounce[marker], label = 'jounce')
 
     plt.xlabel('Frame')
     plt.title('Kinematics over time')
@@ -123,5 +116,5 @@ def plot():
     plt.grid(True)
     plt.show()
 
-find_strikes('RD2P')
-plot()
+find_cycles()
+plot('RD2P')