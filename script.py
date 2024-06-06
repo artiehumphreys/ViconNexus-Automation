462b491fdac6cd1200d8246a208f5399fa365548
Artie Humphreys <artieh2003@gmail.com>
2024-06-06 09:32:34 -0400

    Foot-up works great for trial 06 now

--- script.py
+++ script.py
@@ -22,7 +22,6 @@ right_foot_markers = (
     'RD2P',
     'RD5P',
     'RHEE',
-    'RINFH'
 )
 
 left_foot_markers = (
@@ -31,25 +30,29 @@ left_foot_markers = (
     'LHEE'
 )
 
+markers = left_foot_markers + right_foot_markers
 
-z_coords = {marker: [] for marker in right_foot_markers}
-velo = {marker: [] for marker in right_foot_markers}
-accel = {marker: [] for marker in right_foot_markers}
-jerk = {marker: [] for marker in right_foot_markers}
-jounce = {marker: [] for marker in right_foot_markers}
 
-for marker in right_foot_markers:
+z_coords = {marker: [] for marker in markers}
+velo = {marker: [] for marker in markers}
+accel = {marker: [] for marker in markers}
+jerk = {marker: [] for marker in markers}
+jounce = {marker: [] for marker in markers}
+
+for marker in markers:
     z_coords[marker].extend(vicon.GetTrajectoryAtFrame(subject, marker, frame)[2] for frame in range(user_defined_region[0], user_defined_region[1]))
-for marker in right_foot_markers:   
+for marker in markers:   
     velo[marker].extend(np.gradient(z_coords[marker]))
-for marker in right_foot_markers:   
+for marker in markers:   
     accel[marker].extend(np.diff(z_coords[marker], 2))
-for marker in right_foot_markers:   
+for marker in markers:   
     jerk[marker].extend(np.diff(z_coords[marker], 3))
-for marker in right_foot_markers:   
+for marker in markers:   
     jounce[marker].extend(np.diff(z_coords[marker], 4))
 
 def find_cycles(marker: str = 'RD2P'):
+    foot_down_frames = []
+    foot_up_frames = []
     foot_down = False
     def is_accel_peak(i, threshold: float =5):
         return marker_accel[i] > threshold and marker_accel[i-1] < marker_accel[i] > marker_accel[i+1]
@@ -58,9 +61,12 @@ def find_cycles(marker: str = 'RD2P'):
         return marker_velo[i] < threshold and marker_velo[i-1] > marker_velo[i] < marker_velo[i+1]
     
     def is_velo_peak(i, threshold: float =5):
-        return marker_velo[i] > threshold and marker_velo[i-1] < marker_velo[i] > marker_velo[i+1]
+        return marker_velo[i] > threshold 
+        #and marker_velo[i-1] < marker_velo[i] > marker_velo[i+1]
+    def is_jerk_trough(i, threshold = -1):
+        return marker_jerk[i] < threshold and marker_jerk[i-1] > marker_jerk[i] < marker_jerk[i+1]
  
-    accel_peak = velo_trough = velo_trough_plant = velo_peak = False
+    accel_peak = velo_trough = velo_trough_plant = velo_peak = jerk_trough = False
     marker_accel = accel[marker]
     marker_velo = velo[marker]
     marker_jerk = jerk[marker]
@@ -74,7 +80,8 @@ def find_cycles(marker: str = 'RD2P'):
                 velo_trough = True
 
             if accel_peak and velo_trough and marker_accel[i-1] > marker_velo[i-1] and marker_accel[i] < marker_velo[i]:
-                print("strike at " + str(i-1 + user_defined_region[0]))
+                foot_down_frames.append(i-1 + user_defined_region[0])
+                #print("strike at " + str(i-1 + user_defined_region[0]))
                 accel_peak = velo_trough = False
                 foot_down = True
         else:
@@ -84,15 +91,21 @@ def find_cycles(marker: str = 'RD2P'):
             if is_velo_trough(i, -2.5):
                 velo_trough_plant = True
             
-            if velo_peak and marker_jerk[i] > 0:
-                print("foot up at " + str(i-1 + user_defined_region[0]))
+            if is_jerk_trough(i):
+                jerk_trough = True
+            
+            if velo_peak and jerk_trough:
+                foot_up_frames.append(i+1 + user_defined_region[0])
+                #print("foot up at " + str(i-1 + user_defined_region[0]))
                 velo_peak = velo_trough_plant = False
                 foot_down = False
 
-            elif velo_trough_plant and marker_jerk[i] > 0:
-                print("foot up at " + str(i-1 + user_defined_region[0]))
+            elif velo_trough_plant and jerk_trough:
+                foot_up_frames.append(i+1 + user_defined_region[0])
+                #print("foot up at " + str(i-1 + user_defined_region[0]))
                 velo_peak = velo_trough_plant = False
                 foot_down = False
+    return foot_down_frames, foot_up_frames
 
     
 
@@ -116,5 +129,9 @@ def plot(marker: str = 'RD2P'):
     plt.grid(True)
     plt.show()
 
+# def calculate_cycles(cycles_a, cycles_2, cycles_3)
+
+for marker in right_foot_markers:
+    print(find_cycles(marker))
 find_cycles()
 plot('RD2P')
