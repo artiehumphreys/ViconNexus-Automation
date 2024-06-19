c22c562d735f5257d9b71070bdcce935e82204cb
Artie Humphreys <artieh2003@gmail.com>
2024-06-19 09:49:55 -0400

    fix(plate): method refactor

--- src/plate.py
+++ src/plate.py
@@ -62,54 +62,56 @@ class Plate:
         device_ids = self.vicon.vicon.GetDeviceIDs()
         for device_id in device_ids:
             details = self.vicon.vicon.GetDeviceDetails(device_id)
-            if details[1] == "ForcePlate":
-                wt = details[4].WorldT
-                plate_name = plate_configs.get(tuple(wt))  # type: ignore
-                if plate_name == self.name:
-                    self.wt = wt
-                    self.name = plate_name
-                    self.wr = details[4].WorldR
-                    self.fx = self.vicon.vicon.GetDeviceChannel(device_id, 1, 1)[0]
-                    self.fy = self.vicon.vicon.GetDeviceChannel(device_id, 1, 2)[0]
-                    self.fz = self.vicon.vicon.GetDeviceChannel(device_id, 1, 3)[0]
-                    self.copx = self.vicon.vicon.GetDeviceChannel(device_id, 3, 1)[0]
-                    self.copy = self.vicon.vicon.GetDeviceChannel(device_id, 3, 2)[0]
-                    self.copz = self.vicon.vicon.GetDeviceChannel(device_id, 3, 3)[0]
-                    self.copx_raw = copy.deepcopy(self.copx)
-                    self.copy_raw = copy.deepcopy(self.copy)
-                    self.copz_raw = copy.deepcopy(self.copz)
-                    self.mx = self.vicon.vicon.GetDeviceChannel(device_id, 2, 1)[0]
-                    self.my = self.vicon.vicon.GetDeviceChannel(device_id, 2, 2)[0]
-                    self.mz = self.vicon.vicon.GetDeviceChannel(device_id, 2, 3)[0]
-                    for idx, (a, b) in enumerate(zip(self.copx, self.copy)):
-                        self.copx[idx] = self.wt[0] - b
-                        self.copy[idx] = self.wt[1] + a
-                    strike = self.find_plate_strikes()
-                    return strike
+            wt = details[4].WorldT
+            plate_name = plate_configs.get(tuple(wt))  # type: ignore
+            if details[1] != "ForcePlate" or plate_name != self.name:
+                continue
+            self.wt = wt
+            self.name = plate_name
+            self.wr = details[4].WorldR
+            self.fx = self.vicon.vicon.GetDeviceChannel(device_id, 1, 1)[0]
+            self.fy = self.vicon.vicon.GetDeviceChannel(device_id, 1, 2)[0]
+            self.fz = self.vicon.vicon.GetDeviceChannel(device_id, 1, 3)[0]
+            self.copx = self.vicon.vicon.GetDeviceChannel(device_id, 3, 1)[0]
+            self.copy = self.vicon.vicon.GetDeviceChannel(device_id, 3, 2)[0]
+            self.copz = self.vicon.vicon.GetDeviceChannel(device_id, 3, 3)[0]
+            self.copx_raw = copy.deepcopy(self.copx)
+            self.copy_raw = copy.deepcopy(self.copy)
+            self.copz_raw = copy.deepcopy(self.copz)
+            self.mx = self.vicon.vicon.GetDeviceChannel(device_id, 2, 1)[0]
+            self.my = self.vicon.vicon.GetDeviceChannel(device_id, 2, 2)[0]
+            self.mz = self.vicon.vicon.GetDeviceChannel(device_id, 2, 3)[0]
+            for idx, (a, b) in enumerate(zip(self.copx, self.copy)):
+                self.copx[idx] = self.wt[0] - b
+                self.copy[idx] = self.wt[1] + a
+            strike = self.find_plate_strikes()
+            return strike
 
     def find_plate_strikes(self):
         """Find the frame intervals when a foot is exerting force on a plate"""
         lower, upper = self.vicon.get_region_of_interest()
         strike_intervals = []
         possible_strike = False
-        fc = 0
+        frame_count = 0
         start_frame = end_frame = None
         # 2000hz refresh for plates, 200hz for camera
         for i in range(lower * 10, upper * 10):
             resultant = math.sqrt(self.fx[i] ** 2 + self.fy[i] ** 2 + self.fz[i] ** 2)
-            if resultant > 15:
+            force_threshold = 15
+            frame_threshold = 15
+            if resultant > force_threshold:
                 if not possible_strike:
                     possible_strike = True
                     start_frame = i
-                elif fc > 15:
+                elif frame_count > frame_threshold:
                     end_frame = i
-                fc += 1
+                frame_count += 1
             else:
                 if start_frame and end_frame:
                     strike_intervals.append((start_frame, end_frame))
                 start_frame = None
                 end_frame = None
-                fc = 0
+                frame_count = 0
                 possible_strike = False
         return strike_intervals
 
@@ -134,7 +136,6 @@ class Plate:
                     self.vicon.strike_events[foot][i],
                     (self.vicon.off_events[foot][i] + 1),
                 ):
-                    # foot not in bounds of the plates
                     if not frame_in_strike_interval(j):
                         continue
                     if foot == "left":
